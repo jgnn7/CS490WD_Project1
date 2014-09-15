b791a3926acf368ff69d76e86f371a9269fd7c1e
ryanxjohnson <ryan.x.johnson@gmail.com>
2014-09-14 19:36:38 -0500

    Initial Commit

---
+++ public_html/data/movies.js
@@ -0,0 +1,10 @@
+
+var movies={"movies":[
+                     {"title": "Blue Streak", "year": 1999, "Starting": "Martin Lawrence, Luke Wilso", "HD":true, "genre":"comedy", "photo":"data/blue-Streak.jpg","rating":4},
+                     {"title": "Mrs. Doubtfire", "year": 1993, "Starting": "Robin Williams, Sally Field", "HD":false, "genre":"comedy", "photo":"data/doubt_fire.jpg","rating":5},
+                     {"title": "Life of Pi", "year": 2012, "Starting": "Suraj Sharma, Irrfan Khan", "HD":true, "genre":"drama", "photo":"data/life_of_pi.jpg","rating":5},
+                     {"title": "The Pacific", "year": 2010, "Starting": "James Badge Dale, Joseph Mazzello", "HD":true, "genre":"action", "photo":"data/pacific.jpg","rating":3},
+                     {"title": "Rio", "year": 2011, "Starting": "Jesse Eisenberg, Leslie Mann", "HD":true, "genre":"action", "photo":"data/rio.jpg","rating":4},
+                     {"title": "An Unfinished Life", "year": 2005, "Starting": "Robert Redford, Jennifer Lopez", "HD":false, "genre":"drama", "photo":"data/unfinished_life.jpg","rating":3},
+                     {"title": "The Woman in Black", "year": 2012, "Starting": "Lu Corfield, Daniel Radcliffe", "HD":true, "genre":"horro", "photo":"data/woman_in_black.jpg","rating":5}
+                     ]};
